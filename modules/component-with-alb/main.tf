96b3a7baa97721c081ecc89a0479b46efacd3789
Raghu K <raghuk.vit@gmail.com>
2026-03-27 08:08:09 +0530

    WMP scripts

--- modules/component-with-alb/main.tf
+++ modules/component-with-alb/main.tf
@@ -55,12 +55,12 @@ resource "aws_launch_template" "main" {
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.instance.id, aws_security_group.alb.id]
-  user_data              = filebase64(templatefile("${path.module}/userdata.sh"),
+  user_data              = filebase64(templatefile("${path.module}/userdata.sh",
     {
       ENV       = var.env
       COMPONENT = var.component
     }
-  )
+  ))
 
   tag_specifications {
     resource_type = "instance"
